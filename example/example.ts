757ec281d178db36d78ba5e42d0d7acd39f6d582
Alexander Johansson <alexander@n1s.se>
2025-06-13 15:49:37 +0200

    chore: stackblitz demo

--- example/example.ts
+++ example/example.ts
@@ -1,38 +1,42 @@
 import { parseAsync, stringifyAsync } from "danson";
 
-const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const source = () => ({
 	iterator: (async function* () {
-		await wait(5000);
+		await sleep(5000);
 		yield "hello, i am an iterator";
-		await wait(500);
+		await sleep(500);
 		return "i can return stuff too";
 	})(),
 	normalObject: {
 		foo: "bar",
 	},
 	promise: (async () => {
-		await wait(1000);
+		await sleep(1000);
 		return "hello promise";
 	})(),
 });
 type Source = ReturnType<typeof source>;
 
-// Reconstructing the object
-{
-	const iterator = stringifyAsync(source());
+async function main() {
+	{
+		console.log("Stringifying and parsing the object...");
+		const iterator = stringifyAsync(source());
 
-	const obj = await parseAsync<Source>(iterator);
+		const obj = await parseAsync<Source>(iterator);
 
-	console.log("Recreated object:", obj);
-}
-{
-	console.log("Showing what stringifyAsync returns...");
-	// Showing what stringifyAsync returns
-	const iterator = stringifyAsync(source());
+		console.log("Recreated object:", obj);
+	}
+	{
+		console.log("Showing what stringifyAsync returns...");
+		// Showing what stringifyAsync returns
+		const iterator = stringifyAsync(source());
 
-	for await (const chunk of iterator) {
-		console.dir(JSON.parse(chunk), { depth: null });
+		for await (const chunk of iterator) {
+			console.dir(JSON.parse(chunk), { depth: null });
+		}
 	}
 }
+
+main().catch(console.error);
