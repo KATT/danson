c28f6c03baa0e0a7d0a8f79bb3baaf316b1abe19
Alexander Johansson <alexander@n1s.se>
2025-06-13 15:33:47 +0200

    chore: knip + fix ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
 			include: ["src"],
 			reporter: ["html", "lcov"],
 		},
-		exclude: ["lib", "node_modules", "benchmark"],
+		include: ["./src/**/*.test.*"],
 		setupFiles: process.env.CI ? ["console-fail-test/setup"] : [],
 	},
 });
