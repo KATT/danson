1b2c3cf34831d9a47c51af9e59bcf932661bda6e
Alexander Johansson <alexander@n1s.se>
2025-06-17 15:16:19 +0200

    meow

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 		clearMocks: true,
 		coverage: {
 			all: true,
+			exclude: ["**/test.*.ts", "**/*.bench.ts"],
 			include: ["src"],
 			reporter: ["html", "lcov"],
 		},
