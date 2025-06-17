0c39277ce9f5738f1b2dacc40f9d7faa99134740
Alexander Johansson <alexander@n1s.se>
2025-06-17 16:02:59 +0200

    mkay

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 		clearMocks: true,
 		coverage: {
 			all: true,
-			exclude: ["**/test.*.ts", "**/*.bench.ts"],
+			exclude: ["**/test.*.ts", "**/*.bench.ts", "**/*.test.ts"],
 			include: ["src"],
 			reporter: ["html", "lcov"],
 		},
